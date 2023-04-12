f3b6dccb00f0bbd987128b474b6d863b7a3720bd
chandanswamy <chandanswamy13214@gmail.com>
2023-04-13 00:31:25 +0530

    fourth save

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Projects from './components/Projects'
 import Header from "./components/Header";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+
 import './App.css'
 
 const wallpaperColors = ['original','grey', 'oceanview', 'fire']
@@ -21,6 +24,8 @@ const App = () => (
         <Route exact path="/about" element={<About/>} />
         <Route exact path="/contact" element={<Contact/>}  />
         <Route exact path="/projects" element={<Projects/>}  />
+        <Route exact path="/about/education" element={<Education />} />
+        <Route exact path="/about/experience" element={<Experience />} />
       </Routes>
     </div>
   </div>
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -16,60 +16,64 @@ import './index.css'
 
 const Skills = () => {
   return (
-    <div>
+    <div className='skill-section'>
         <h1 className='skills-heading'>Skills and Certifications</h1>
-    <div className='skills-section'>
-        <Link className='skill-icon-link' to="./PEVHLMTRXV.pdf" target='_blank'>
-        <div className='skill-icons-card'>
-            <FaHtml5 className='skill-icon'/>
-        </div >
-        </Link>
-        <Link className='skill-icon-link' to="./IINZYRFELJ.pdf" target='_blank'>
-        <div className='skill-icons-card'>
-            <FaCss3Alt className='skill-icon'/>
+        <div className='skill-image-container'>
+            <img src="/skills101.png" className='skill-image'  alt="skill development"/>
+            <img src="/skills102.png" className='skill-image image-two'  alt="skill development"/>
         </div>
-        </Link>
-        <Link className='skill-icon-link' to="./RXCTRLMHUK.pdf" target='_blank'>
-        <div className='skill-icons-card'>
-            <SiMysql className='skill-icon'/>
+        <div className='skills-section'>
+            <Link className='skill-icon-link' to="./PEVHLMTRXV.pdf" target='_blank'>
+            <div className='skill-icons-card'>
+                <FaHtml5 className='skill-icon'/>
+            </div >
+            </Link>
+            <Link className='skill-icon-link' to="./IINZYRFELJ.pdf" target='_blank'>
+            <div className='skill-icons-card'>
+                <FaCss3Alt className='skill-icon'/>
+            </div>
+            </Link>
+            <Link className='skill-icon-link' to="./RXCTRLMHUK.pdf" target='_blank'>
+            <div className='skill-icons-card'>
+                <SiMysql className='skill-icon'/>
+            </div>
+            </Link>
+            <Link className='skill-icon-link' to="./VWXIZWMCNY.pdf" target='_blank'>
+            <div className='skill-icons-card'>
+                <FaPython className='skill-icon'/>
+            </div>
+            </Link>
+            <Link className='skill-icon-link' to="./GVQJKGYIFV.pdf" target='_blank'>
+            <div className='skill-icons-card'>
+                <IoLogoJavascript className='skill-icon'/>
+            </div>
+            </Link>
+            <Link className='skill-icon-link' to="./OIBEOOEABU.pdf" target='_blank'>
+            <div className='skill-icons-card'>
+                <FaBootstrap className='skill-icon'/>
+            </div>
+            </Link>
+            <Link className='skill-icon-link' to="./UEUIXIUHDA.pdf" target='_blank'>
+            <div className='skill-icons-card'>
+                <BsGit className='skill-icon'/>
+            </div>
+            </Link>
+            <Link className='skill-icon-link' to="./UEUIXIUHDA.pdf" target='_blank'>
+            <div className='skill-icons-card'>
+                <FaGithub className='skill-icon'/>
+            </div>
+            </Link>
+            <Link className='skill-icon-link' >
+            <div className='skill-icons-card'> 
+                <FaReact className='skill-icon'/>
+            </div>
+            </Link>
+            <Link className='skill-icon-link' to="./SNCQMPBYZJ.pdf" target='_blank'>
+            <div className='skill-icons-card'>
+                <FaNodeJs className='skill-icon'/>
+            </div>
+            </Link>
         </div>
-        </Link>
-        <Link className='skill-icon-link' to="./VWXIZWMCNY.pdf" target='_blank'>
-        <div className='skill-icons-card'>
-            <FaPython className='skill-icon'/>
-        </div>
-        </Link>
-        <Link className='skill-icon-link' to="./GVQJKGYIFV.pdf" target='_blank'>
-        <div className='skill-icons-card'>
-            <IoLogoJavascript className='skill-icon'/>
-        </div>
-        </Link>
-        <Link className='skill-icon-link' to="./OIBEOOEABU.pdf" target='_blank'>
-        <div className='skill-icons-card'>
-            <FaBootstrap className='skill-icon'/>
-        </div>
-        </Link>
-        <Link className='skill-icon-link' to="./UEUIXIUHDA.pdf" target='_blank'>
-        <div className='skill-icons-card'>
-            <BsGit className='skill-icon'/>
-        </div>
-        </Link>
-        <Link className='skill-icon-link' to="./UEUIXIUHDA.pdf" target='_blank'>
-        <div className='skill-icons-card'>
-            <FaGithub className='skill-icon'/>
-        </div>
-        </Link>
-        <Link className='skill-icon-link' >
-        <div className='skill-icons-card'> 
-            <FaReact className='skill-icon'/>
-        </div>
-        </Link>
-        <Link className='skill-icon-link' to="./SNCQMPBYZJ.pdf" target='_blank'>
-        <div className='skill-icons-card'>
-            <FaNodeJs className='skill-icon'/>
-        </div>
-        </Link>
-    </div>
     </div>
   )
 }
